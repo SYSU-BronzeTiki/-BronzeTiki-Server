e6a38642997a7235549431bcbc564a7e50279b2e
chenxfeng <chenxf47@mail2.sysu.edu.cn>
2018-06-08 14:11:56 +0800

    deploy front-end test

--- app.py
+++ app.py
@@ -1,22 +1,39 @@
 #encoding: utf-8
 
 from model import *
-from flask import render_template, url_for
+from flask import render_template, url_for, jsonify
+from random import *
 
 # create data tables
 db.create_all()
 
-# import os
-# indexFilePath = os.getcwd()
-# indexFilePath = os.path.join(indexFilePath, 'templates\index.html')
+@app.route('/api/userRegister')
+def user_register():
+    pass
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-    # indexStr = ''
-    # with open(indexFilePath, 'r') as f:
-    #     indexStr = f.read()''
-    # return indexStr
+@app.route('/api/userLogin')
+def user_login():
+    pass
+
+@app.route('/api/userLogout')
+def user_logout():
+    pass
+
+# @app.route('/api/random')
+# def random_number():
+#     response = {
+#         'randomNumber': randint(1, 100)
+#     }
+#     return jsonify(response)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 if __name__ == '__main__':
     debug=True
--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import config
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./dist/static', template_folder='./dist')
 app.config.from_object(config)
 db = SQLAlchemy(app)
 
