196a9ee57659ac3184a027891f7c8b86a4c339cd
chenxfeng <chenxf47@mail2.sysu.edu.cn>
2018-06-18 13:39:39 +0800

    add api

--- app.py
+++ app.py
@@ -8,108 +8,145 @@ import hashlib, random, time
 # create data tables
 db.create_all()
 
-def valid_register(username, password, jsonData):
+def register_a_user(username, password, jsonData):
     result = User.query.filter(User.username == username).all()
     if len(result) == 0:
-        return True
+        try:
+            md5psw = str(hashlib.md5((salt + password).encode()).digest())
+            user = User(username=username, password=md5psw)
+            db.session.add(user)
+            db.session.commit()
+            jsonData['message'] = 'Register succeeded'
+            return True
+        except:
+            jsonData['message'] = 'Register failed'
+            return False
     else:
-        jsonData['reason'] = 'Username existed'
+        jsonData['message'] = 'Username existed'
         return False
-
-def register_a_user(username, password, jsonData):
-    user = User(username=username, password=password)
-    db.session.add(user)
-    db.session.commit()
-    return True
-
-def valid_login(username, password, jsonData):
-    result = User.query.filter(User.username == username).first()
-    if result and result.password == password:
-        return True
-    else:
-        jsonData['reason'] = 'Invalid username/password'
-        return False
-def login_the_user(username, jsonData):
-    result = User.query.filter(User.username == username).first()
-    jsonData['avator'] = result.avator
-    jsonData['nickname'] = result.nickname
-    jsonData['description'] = result.description
-    jsonData['money'] = result.money
-    return True
 
 @app.route('/api/users/register', methods=['POST', 'GET'])
 def user_register():
-    jsonData = {}
-    jsonData['timestamp'] = time.time()
-    if request.method == 'POST':
-        if 'username' in request.form and 'password' in request.form:
-            md5psw = str(hashlib.md5((salt + request.form['password']).encode()).digest())
-            if valid_register(request.form['username'], md5psw, jsonData):
-                register_a_user(request.form['username'], md5psw, jsonData)
-                jsonData['status'] = 200
-            else:
-                jsonData['status'] = 0
-
-        print(str(jsonData))
-        # message = str(jsonData)
-        message = json.dumps(jsonData)
-        return message
-    else:
-        return redirect(url_for('front_end'))
-
-@app.route('/api/users/login', methods=['POST', 'GET'])
-def user_login():
-    jsonData = {}
-    jsonData['timestamp'] = time.time()
-    if request.method == 'POST':
-        if 'username' in session:
-            jsonData['uid'] = session['uid']
-            return redirect(url_for('front_end'))
-        elif 'username' in request.form and 'password' in request.form:
-            md5psw = str(hashlib.md5((salt + request.form['password']).encode()).digest())
-            if valid_login(request.form['username'], md5psw, jsonData):
-                login_the_user(request.form['username'], jsonData)           
-                session['username'] = request.form['username']
-                session['uid'] = random.randint(100000, 999999)
-                jsonData['uid'] = session['uid']
-                jsonData['status'] = 200
-            else:
-                jsonData['status'] = 0
-
-        print(str(jsonData))
-        # message = str(jsonData)
-        message = json.dumps(jsonData)
-        return message
-    else:
-        return redirect(url_for('front_end'))
-
-@app.route('/api/user/logout', methods=['POST', 'GET'])
-def user_logout():
     if request.method == 'POST':
         jsonData = {}
         jsonData['timestamp'] = time.time()
-        if request.args.get('logout'):
-            session.clear()
-            jsonData['status'] = 200
 
-        print(str(jsonData))
-        # message = str(jsonData)
-        message = json.dumps(jsonData)
+        if 'username' not in request.form:
+            jsonData['status'] = '0'
+            jsonData['message'] = 'No username'
+        elif 'password' not in request.form:
+            jsonData['status'] = '0'
+            jsonData['message'] = 'No password'
+        else:
+            # validate and register
+            if register_a_user(request.form['username'], request.form['password'], jsonData):
+                jsonData['status'] = 200
+            else:
+                jsonData['status'] = 0
+
+        print(str(jsonData))           # debug the message
+        message = json.dumps(jsonData) # convert to json
         return message
-    else:
-        return redirect(url_for('front_end'))
+    # GET
+    return redirect(url_for('front_end'))
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+def login_the_user(username, password, jsonData):
+    result = User.query.filter(User.username == username).first()
+    md5psw = str(hashlib.md5((salt + password).encode()).digest())
+    if result and result.password == md5psw:
+        try:
+            result = User.query.filter(User.username == username).first()
+            jsonData['avator'] = result.avator
+            jsonData['nickname'] = result.nickname
+            jsonData['description'] = result.description
+            jsonData['money'] = result.money
+            jsonData['reason'] = 'Login succeeded'
+            return True
+        except:
+            jsonData['reason'] = 'Login failed'
+            return False
+    else:
+        jsonData['reason'] = 'Invalid username/password'
+        return False
+
+@app.route('/api/users/login', methods=['POST', 'GET'])
+def user_login():
+    # POST
+    if request.method == 'POST':
+        jsonData = {}
+        jsonData['timestamp'] = time.time()
+
+        if 'username' not in request.form:
+            jsonData['status'] = '0'
+            jsonData['message'] = 'No username'
+        elif 'password' not in request.form:
+            jsonData['status'] = '0'
+            jsonData['message'] = 'No password'
+        else:
+            # validate and login
+            if login_the_user(request.form['username'], request.form['password'], jsonData):
+                session['username'] = request.form['username']
+                session['avator'] = jsonData['avator']
+                session['nickname'] = jsonData['nickname']
+                session['description'] = jsonData['description']
+                session['money'] = jsonData['money']
+                session.permanent = True
+                # session['uid'] = (random.randint(1000000, 9999999) + (int)(time.time())) % 1000000
+                # jsonData['uid'] = session['uid']
+                jsonData['status'] = 200
+            else:
+                jsonData['status'] = 0
+
+        print(str(jsonData))           # debug the message
+        message = json.dumps(jsonData) # convert to json
+        return message
+    # GET
+    if 'username' in session:
+        if request.args.get('logout'):
+            print(session.get('username', 'none') + " logout")
+            session.clear()
+        return redirect(url_for('front_end'))
+    # GET
+    return redirect(url_for('front_end'))
+
+@app.route('/api/movies', methods=['POST', 'GET'])
+def movies():
+    pass
+
+@app.route('/api/orders', methods=['POST', 'GET'])
+def orders():
+    pass
+
+@app.route('/api/state', methods=['GET', 'POST'])
+def state():
+    jsonData = {}
+    jsonData['timestamp'] = time.time()
+    if 'username' not in session:
+        jsonData['message'] = 'Offline'
+        jsonData['status'] = 0
+    elif 'username' in session:
+        # jsonData['uid'] = jsonData['uid']
+        jsonData['username'] = session.get('username', 'none')
+        jsonData['avator'] = session.get('avator', 'none')
+        jsonData['nickname'] = session.get('nickname', 'none')
+        jsonData['description'] = session.get('description', 'none')
+        jsonData['money'] = session.get('money', 'none')
+        jsonData['message'] = 'Online'
+        jsonData['status'] = 200
+    print(str(jsonData))           # debug the message
+    message = json.dumps(jsonData) # convert to json
+    return message
+
+
+@app.route('/', defaults={'path': ''})  # root dir
+@app.route('/<path:path>')              # any path
 def front_end(path):
     if not request.headers.get('User-Agent'):
         abort(403)
 
+    paths = path.split('/')
+    print([x for x in paths])
     return render_template("index.html")
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
 
 if __name__ == '__main__':
     debug=True
